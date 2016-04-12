ae5d57be23c23778349c0da5b65398715deb0e62
yuhe <gfgkmn@gmail.com>
2016-04-12 07:25:53 +0000

    change zsh

--- init_environment.sh
+++ init_environment.sh
@@ -1,5 +1,6 @@
+sudo usermod -s /bin/zsh $USER
 mkdir .vimbakfiles
 mkdir -p .vim/vundles/
 cd .vim/vundles
 git clone https://github.com/VundleVim/Vundle.vim.git
-vim -c 'PluginList' -c qa
+vim -c 'PluginUpdate' -c qa
