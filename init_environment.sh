a4afb1630a869238be61d1f8cc3b634eac7af0b2
yuhe <gfgkmn@gmail.com>
2016-04-14 03:25:59 +0000

    add facebook pathpicker

--- init_environment.sh
+++ init_environment.sh
@@ -2,6 +2,9 @@ sudo usermod -s /bin/zsh $USER
 mkdir ~/.vimbakfiles
 mkdir -p ~/.vim/vundles/
 mkdir -p ~/Application/bin
+git clone https://github.com/facebook/PathPicker.git
+mv PathPicker ~/Application
+ln -s ~/Application/PathPicker/fpp ~/Application/bin/fpp
 cp ./j ~/Application/bin
 cp ./z.sh ~/Application/bin
 cd ~/.vim/vundles
