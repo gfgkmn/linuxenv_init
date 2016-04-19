2789a54a7835ea80cdddaa87c82b19206265aeeb
gfgkmn <gfgkmn@gmail.com>
2016-04-19 15:16:38 +0800

    inputrc

--- init_environment.sh
+++ init_environment.sh
@@ -8,6 +8,7 @@ ln -s ~/Application/PathPicker/fpp ~/Application/bin/fpp
 mkdir -p ~/.ipython/profile_default
 cp ipython_config.py ~/.ipython/profile_default
 cp ./j ~/Application/bin
+cp ./.inputrc ~/
 cp ./z.sh ~/Application/bin
 cd ~/.vim/vundles
 git clone https://github.com/VundleVim/Vundle.vim.git
