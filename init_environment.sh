29626279bb5e23b310516d04ffd81df63d4b861d
gfgkmn <gfgkmn@gmail.com>
2020-03-24 09:45:34 +0800

    delete without git, move software download seperately

--- init_environment.sh
+++ init_environment.sh
@@ -104,18 +104,6 @@ fi
 git config --global user.email "gfgkmn@gmail.com"
 git config --global user.name "yuhe"
 
-# wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-wget https://repo.continuum.io/miniconda/Miniconda3-4.5.4-Linux-x86_64.sh
-wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
-
-# bash Miniconda2-latest-Linux-x86_64.sh
-# bash Miniconda3-latest-Linux-x86_64.sh
-#
-git clone https://github.com/vim/vim.git
-
-# wget https://www.kernel.org/pub/software/scm/git/git-2.16.2.tar.xz
-
-# pip install -r requirements.txt
 
 git clone git@github.com:universal-ctags/ctags.git
 cd ctags
