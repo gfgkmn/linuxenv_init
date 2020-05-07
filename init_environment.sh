de05ca33a7626c882f85032091823847ec22cefc
yuhe <gfgkmn@gmail.com>
2020-05-07 16:12:01 +0800

    update bashrc

--- init_environment.sh
+++ init_environment.sh
@@ -90,9 +90,7 @@ fi
 ssh-keygen -t rsa -N ''
 
 cat >> ~/.ssh/authorized_keys << eof
-ssh-rsa
-AAAAB3NzaC1yc2EAAAABIwAAAQEA0ZzwzAjt7F4xOD4E0TKreAVUQKC8htT2n0DsjVximdIeaiXB24RDmYR7Hv05Iu9Mbc4K/MRrEMbKSBTEr15MN5LKZFNWpmC2V0ur5iq+hsPGMZoQ8ixB+YAmBw00I3qkEG/ceEaVX7zXLffLo+oahJEyYRWOIAUIW1Cligfs90OljX/lvzbVC+UASK950eAKWaTFlTVW1VKz6uhGOzqlbZBI+lIN1G0bLU+14XCz3rvlv2dgZCEuLZWEpC55iQllfJirmohjBBOuw7StbvRH4bLTne12ahoXDGpM0Bflawa8werv/Qp0/ib4vHeUV7sZu4STKkADjQP7ByiZwgrhcw==
-gfgkmn@gmail.com
+ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA0ZzwzAjt7F4xOD4E0TKreAVUQKC8htT2n0DsjVximdIeaiXB24RDmYR7Hv05Iu9Mbc4K/MRrEMbKSBTEr15MN5LKZFNWpmC2V0ur5iq+hsPGMZoQ8ixB+YAmBw00I3qkEG/ceEaVX7zXLffLo+oahJEyYRWOIAUIW1Cligfs90OljX/lvzbVC+UASK950eAKWaTFlTVW1VKz6uhGOzqlbZBI+lIN1G0bLU+14XCz3rvlv2dgZCEuLZWEpC55iQllfJirmohjBBOuw7StbvRH4bLTne12ahoXDGpM0Bflawa8werv/Qp0/ib4vHeUV7sZu4STKkADjQP7ByiZwgrhcw== gfgkmn@gmail.com
 eof
 
 chmod 600 ~/.ssh/authorized_keys
@@ -108,6 +106,6 @@ git config --global user.name "yuhe"
 git clone git@github.com:universal-ctags/ctags.git
 cd ctags
 ./autogen.sh
-./configure --prefix=~/Application --enable-json # defaults to /usr/local
+./configure --prefix=/home/yuhe/Application --enable-json # defaults to /usr/local
 make
 make install # may require extra privileges depending on where to install
